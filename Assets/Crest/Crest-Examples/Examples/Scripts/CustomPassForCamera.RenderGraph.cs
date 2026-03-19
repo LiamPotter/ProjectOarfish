7ab807337a10177b4068255560c133a7bfff40ff
Liam Potter <liamp62@gmail.com>
2026-03-19 18:33:48 +1000

    Submitting Crest Plugin and initial tests

---
+++ Assets/Crest/Crest-Examples/Examples/Scripts/CustomPassForCamera.RenderGraph.cs
@@ -0,0 +1,89 @@
+// Crest Ocean System
+
+// Copyright 2024 Wave Harmonic Ltd
+
+#if CREST_URP
+#if UNITY_6000_0_OR_NEWER
+
+namespace Crest.Examples
+{
+    using UnityEngine;
+    using UnityEngine.Rendering;
+    using UnityEngine.Rendering.RenderGraphModule;
+    using UnityEngine.Rendering.Universal;
+
+    partial class CustomPassForCameraBase
+    {
+        partial class CustomPassURP
+        {
+            class PassData
+            {
+                public UniversalCameraData cameraData;
+                public RenderGraphHelper.Handle colorTargetHandle;
+                public RenderGraphHelper.Handle depthTargetHandle;
+
+                public void Init(ContextContainer frameData, IUnsafeRenderGraphBuilder builder = null)
+                {
+                    var resources = frameData.Get<UniversalResourceData>();
+                    cameraData = frameData.Get<UniversalCameraData>();
+
+#if URP_COMPATIBILITY_MODE
+                    if (builder == null)
+                    {
+#pragma warning disable CS0618 // Type or member is obsolete
+                        colorTargetHandle = cameraData.renderer.cameraColorTargetHandle;
+                        depthTargetHandle = cameraData.renderer.cameraDepthTargetHandle;
+#pragma warning restore CS0618 // Type or member is obsolete
+                    }
+                    else
+#endif
+
+                    {
+                        colorTargetHandle = resources.activeColorTexture;
+                        depthTargetHandle = resources.activeDepthTexture;
+                    }
+                }
+            }
+
+            readonly PassData passData = new();
+
+            public override void RecordRenderGraph(RenderGraph graph, ContextContainer frame)
+            {
+                using (var builder = graph.AddUnsafePass<PassData>(PassName, out var data))
+                {
+                    data.Init(frame, builder);
+                    builder.AllowPassCulling(false);
+
+                    builder.SetRenderFunc<PassData>((data, context) =>
+                    {
+                        var buffer = CommandBufferHelpers.GetNativeCommandBuffer(context.cmd);
+                        ExecutePass(context.GetRenderContext(), buffer, data);
+                    });
+                }
+            }
+
+#if URP_COMPATIBILITY_MODE
+            [System.Obsolete]
+            public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+            {
+                // This will execute in edit mode always, but if an event callback is not set to "Editor And Runtime"
+                // then the entire thing will no longer work in play mode. I believe this is because it will execute
+                // lots of nothing which either triggers a bug or Unity ignores it as an optimisation.
+                if (!Application.isPlaying)
+                {
+                    return;
+                }
+
+                passData.Init(renderingData.GetFrameData());
+                var buffer = CommandBufferPool.Get(PassName);
+                ExecutePass(context, buffer, passData);
+                context.ExecuteCommandBuffer(buffer);
+                CommandBufferPool.Release(buffer);
+            }
+#endif
+        }
+    }
+}
+
+#endif
+#endif
